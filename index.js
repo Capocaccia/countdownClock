778cb870a73dd5dafcfcfdb3a10d82e773d952b2
Carter Capocaccia <Carter.Capocaccia@Gmail.com>
2018-01-04 13:03:35 -0600

    adds functionality to take time from input field and refactors some functionality

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const timerButton = document.querySelectorAll('.timer__button');
+const minuteForm = document.querySelector('[name="minutes"]');
+
+minuteForm.addEventListener('change', (e) => {
+	if(e.target.value.trim() == ''){
+		return;
+	}
+	clearCountdown();
+	restartTimer(e.target.value * 60)
+});
 
 timerButton.forEach((button) => {
-	button.addEventListener('click', function(e){
-		clearInterval(countdown);
-		timerDisplay.textContent = '0:00'
-		timer(e.target.dataset.time);
+	button.addEventListener('click', (e) => {
+		clearCountdown();
+		restartTimer(e.target.dataset.time);
 	});
 });
 
+//clears the interval and resets the view content
+const clearCountdown = () => {
+	clearInterval(countdown);
+	timerDisplay.textContent = '0:00'
+}
+
+//clears the interval, resets view content, restarts timer with new time
+const restartTimer = (time) => {
+	clearCountdown();
+	timer(time);
+}
+
+//starts the timer
 const timer = (seconds) => {
 	const now = Date.now();
 	const then = now + seconds * 1000;
@@ -23,11 +44,10 @@ const timer = (seconds) => {
 
 }
 
+//handles changing the view
 function displayTimeLeft(secondsLeft){
 	const minutes = Math.floor(secondsLeft / 60);
 	const seconds = secondsLeft % 60;
 	const display = `${minutes}:${seconds}`
 	timerDisplay.textContent = display;
 }
-
-timer(10);
